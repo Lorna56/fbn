4b16ad4391269b6563bfd215365705c7184b9dbd
Lorna56 <lornanaula0042@gmail.com>
2025-04-02 23:25:54 +0300

    Add upper bound limit test case

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,7 @@
-const fibonacci = (num) => {
-    
-    return num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2);
-  };
-  
+ const fibonacci = (num) => {
+        if (num > 40) {
+          throw new Error("Number must be equal or less than 40");
+        }
+        return num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2);
+      };
   export default fibonacci;
--- test/test.js
+++ test/test.js
@@ -14,5 +14,10 @@ describe("Fibonacci Function tests for valid integers", () => {
     assert.equal(fibonacci(10), 55);
   });
 });
+describe("Fibonacci function tests for invalid integers", () => {
+  it("Should throw an error for fibonacci(41)", () => {
+    assert.throws(() => fibonacci(41), Error);
+  });
+});
 
 
